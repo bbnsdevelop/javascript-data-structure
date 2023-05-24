a0e54079256d9286a1a50eaa0eebf5b38973e8b5
bbnsdevelop <bbnsdevelop@gmail.com>
2023-05-23 21:35:54 -0300

    add break when found the item

--- loops/for.js
+++ loops/for.js
@@ -1,11 +1,22 @@
 const nemo = ['nemo']
+const everyone = ['dory', 'bruce', 'marlin', 'nemo', 'gill', 'bloat', 'nigel', 'squirt', 'darla', 'hank']
+const large = Array(10000).fill('nemo');
 
-function findNemo(array){    
+function findNemo(array){  
+    let time0 = performance.now();  
+   
     for(let i = 0; i < array.length; i++){
         if(array[i] === 'nemo'){
-            console.log('Found Nemo');
+            console.log('Found Nemo'); 
+            break    
         }
     }
+
+    let time1 = performance.now();
+
+    console.log(`Call to find nem took ${(time1 - time0)} milliseconds`)
 }
 
-findNemo(nemo);
+// findNemo(nemo);
+// findNemo(everyone);
+findNemo(large);
