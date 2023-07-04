d206debcb6606d03e6f763607bf0a91822f89f93
bbnsdevelop <bbnsdevelop@gmail.com>
2023-07-03 22:05:37 -0300

    reverse string

---
+++ reverse-string.js
@@ -0,0 +1,29 @@
+function reverse(){
+    let name = "Bruno";
+    let convertToArray = name.split("");
+    let b = [];
+    for(i = convertToArray.length -1; i >= 0; i--){
+        if(i === (convertToArray.length -1)){
+            b.push(convertToArray[i].toLocaleUpperCase());
+        }
+        else if(i === 0){
+            b.push(convertToArray[i].toLocaleLowerCase());
+        }
+        else{
+            b.push(convertToArray[i]);
+        }
+    }
+
+    console.log(b.join(""));
+}
+
+
+function reverse1(){
+    let name = "Bruno";
+    console.log(name.split("").reverse().join(""));
+}
+
+
+
+reverse();
+reverse1();
