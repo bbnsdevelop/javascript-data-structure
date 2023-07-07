b1b9b8da1ed74135b1e5cce565e6a1fec12ba409
bbnsdevelop <bbnsdevelop@gmail.com>
2023-07-06 22:50:17 -0300

    Recurring Character

---
+++ recurringNumber.js
@@ -0,0 +1,22 @@
+function recurringNumber(input) {
+    let map = {};
+    for (let i = 0; i < input.length; i++) {
+        if (map[input[i]] !== undefined) {
+        return input[i]
+        } else {
+        map[input[i]] = i;
+        }
+    }
+    return undefined
+}
+
+
+
+
+array1 = [2,5,1,2,3,5,1,2,4];
+array2 = [2,1,1,2,3,5,1,2,4];
+array3 = [2,3,4,5];
+
+console.log(recurringNumber(array1));
+console.log(recurringNumber(array2));
+console.log(recurringNumber(array3));
