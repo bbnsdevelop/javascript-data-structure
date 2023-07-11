9740e41de135e3c97361c0652bb8af08c2996173
bbnsdevelop <bbnsdevelop@gmail.com>
2023-07-10 22:40:02 -0300

    insert

--- own-linked-list-javascript.js
+++ own-linked-list-javascript.js
@@ -29,6 +29,49 @@ class LinkedList {
         return this;
     }
 
+    printList() {
+        const array = [];
+        let currentNode = this.head;
+        while(currentNode !== null){
+            array.push(currentNode.value)
+            currentNode = currentNode.next
+        }
+        return array;
+    }
+    
+    insert(index, value){
+        if (index === 0) {
+            this.prepend(value);
+        }
+
+        if(index >= this.length){
+            this.append(value);
+        }
+        else{
+            const newNode = new Node(value);    
+            const leader = this.traverseToIndex(index-1);
+            const holdingPointer = leader.next;
+            leader.next = newNode;
+            newNode.next = holdingPointer;
+            this.length++;
+
+        }
+
+        return this.printList();
+    }
+
+
+    traverseToIndex(index) {
+        let counter = 0;
+        let currentNode = this.head;
+        while(counter !== index){
+          currentNode = currentNode.next;
+          counter++;
+        }
+        return currentNode;
+      }
+    
+
   }
   
 let myLinkedList = new LinkedList(10);
@@ -36,6 +79,7 @@ console.log('First time', myLinkedList);
 
 myLinkedList.append(5);
 myLinkedList.append(16);
-myLinkedList.prepend(1)
-
-console.log('After append', myLinkedList);
+myLinkedList.prepend(1);
+myLinkedList.insert(2, 99);
+ 
+console.log("after insert", myLinkedList.insert(20, 88));
