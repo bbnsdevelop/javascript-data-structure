4b9824d8f2ae66ca727573734c5fb458ee17e9d8
bbnsdevelop <bbnsdevelop@gmail.com>
2023-07-23 19:07:29 -0300

    subtrair

--- soma.js
+++ soma.js
@@ -3,8 +3,14 @@ function somar(a, b){
     return a + b;
 }
 
+function subtrair(a, b){
+    return a - b;
+}
 
-let a = 10;
+
+let a = 100;
 let b = 90;
 
 console.log(somar(a, b));
+
+console.log(subtrair(a, b));
